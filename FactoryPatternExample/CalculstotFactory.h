fe2bc858fb5eb7afebb5947213921a315f6a60fa
Noyce2018 <tianyan_appteam_tbl@tbl.local>
2018-11-02 16:23:44 +0800

    计算器逻辑部分头文件迁入，计算器为抽象类，加减继承于抽象类

---
+++ FactoryPatternExample/CalculstotFactory.h
@@ -0,0 +1,12 @@
+//
+//  CalculstotFactory.h
+//  FactoryPatternExample
+//
+//  Created by TianYan_AppTeam_TBL on 2018/11/2.
+//  Copyright © 2018年 TianYan_AppTeam_TBL. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+#import <objc/message.h>
+@interface CalculatorFactory:NSObject
++(Calculator*)calculatorFactory:(NSString*)operation;
+@end
---
+++ FactoryPatternExample/Sub.h
@@ -0,0 +1,12 @@
+//
+//  Sub.h
+//  FactoryPatternExample
+//
+//  Created by TianYan_AppTeam_TBL on 2018/11/2.
+//  Copyright © 2018年 TianYan_AppTeam_TBL. All rights reserved.
+//
+#import "CalculatorFactory.h"
+@interface Sub:Calculator
+-(int)getResult:(int)x:(int)y;
+@end
+
